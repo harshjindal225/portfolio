c3038c96e636b0f976b2fbf73549c032c5dab1ab
Harsh jindal <92637445+harshjindal225@users.noreply.github.com>
2024-07-23 00:26:55 +0530

    commi

--- contact.php
+++ contact.php
@@ -25,6 +25,7 @@
             font-size: 90px;
             font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
         }
+        
 
         /* Increase text size for h4 headings */
         h4 {
